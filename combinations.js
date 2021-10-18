7bf145fed034ec91e6c92b67b4c928461e4a04f3
ChristosKehayas <christos.kechagia@gmail.com>
2021-10-18 23:31:20 +0100

    Initial Commit

---
+++ combinations.js
@@ -0,0 +1,81 @@
+
+function getAllCombinationsOfASet(text) {
+    var results = [];
+    for (var i = 0; i < text.length; i++) {
+      // Record size as the list will change
+      var resultsLength = results.length;
+      for (var j = 0; j < resultsLength; j++) {
+        results.push(text[i] + results[j]);
+      }
+      results.push(text[i]);
+    }
+
+    //console.log(results[14].split(""));
+    //console.log(parseInt(results[15].split("")[0]));
+
+    const splitter= results
+                    .map(x=>(x.split("")));
+
+    const combos = splitter  
+                    .map(x=>x.map(z=>parseInt(z)));
+
+
+   // const d= ar.concat(combos[0],combos[2],combos[6]);
+
+  
+
+    console.log(combos);
+    //console.log(combos.length);
+    //console.log(combos[6][6]);
+
+ let inspector=[];
+ 
+
+    for(let i =0; i<combos.length-1;i++){
+          
+             
+            for (let z=i+1;z<combos.length;z++){
+                     //console.log(i+""+z);
+              if (!inspector[i]) inspector[i] = [] 
+
+                for(let j=0;j<combos[i].length;j++){
+                          
+                  for(let k=0;k<combos[z].length;k++){
+                    
+                    if(combos[i][j]!==combos[z][k]){  
+                      
+                      //match i and z
+                      inspector[i][z]=z;
+
+                      }else{
+
+                      inspector[i][z]="";
+                      
+                            }
+                                                      }
+                                                    }
+
+                      //} 
+
+                                            }
+           
+        }
+
+      console.log(inspector);
+        
+        return combos;   
+               
+    }
+  
+    
+  
+    getAllCombinationsOfASet("123");
+    
+
+//   const dummyF=()=>
+//   {
+//         const combos = getAllCombinationsOfASet("1234");
+//   }
+// //dummyF();
+
+
---
+++ logic.js
@@ -0,0 +1,80 @@
+
+const paySettle = ()=>{
+
+let maping= {
+                humans:["A","B","C","D","E","F","G","H"], 
+                moneySpent:[10,0,20,0,40]
+            };
+
+const target = maping.moneySpent.reduce(
+    (accumulator, a) => accumulator+a  
+                  ,0)/maping.moneySpent.length; //reduce reduces an array to a single digit
+                                                //target is the amount to be reached by all participants
+
+//let debt=maping.moneySpent.map(x=>target-x);  //loops through all elements of array and calculates
+  let debt=[-14,-10,-8,3,5,7,8,9];                                              // differences from target
+                                                //in order to identify who owns (positive)                                         
+                                                // and who is owned (negative)   
+
+let stateC=0;
+let stateD=0;
+let StateDim=-1;
+let negtracker=-1;
+let postracker=-1;
+let Remainder=0;
+let storage=0;
+let State=[];
+
+
+for (let i=0;i<debt.length;i++){
+    if (debt[i]<0){
+        negtracker=i;
+        stateC++;
+        
+    }//end if
+    
+    for (let j=0;j<debt.length;j++){
+        if(debt[j]>0){
+           postracker=j;
+           stateC++
+            
+         
+        } //end if
+
+        if (stateC>stateD && negtracker!==-1 && postracker!==-1 && debt[negtracker]!==0 && debt[postracker]!==0){
+
+                //stateC updates when a positive or negative balance has been spotted.
+                //This is not enough though - it might update StateC without updating the +ve or -ve trackers (thus returnig NaNs)
+                //We also don't have to loop when debt[X] is zero as acount balance has been settled.
+                
+                StateDim++
+                stateD=stateC;
+                Remainder=debt[negtracker]+debt[postracker];
+
+               
+                if(Remainder<=0){
+                    storage =debt[postracker];
+                    debt[negtracker]=Remainder;
+                    debt[postracker]=0;
+                    State[StateDim]=maping.humans[postracker]+ "--> "+ maping.humans[negtracker]+" " +storage;
+                   
+                    
+                    } else {
+                        storage=-debt[negtracker];
+                        debt[negtracker]=0;
+                        debt[postracker]=Remainder;
+                        State[StateDim]=maping.humans[postracker]+ "--> "+ maping.humans[negtracker]+ " "+storage;
+                 
+                     
+                    }
+                    //end if 
+        }//end if
+    }//Next j
+}//Next i
+
+ console.log(State);
+ return State;
+
+}
+
+paySettle();
